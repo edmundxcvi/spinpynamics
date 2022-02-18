aee988c9cdc033101692311bab1c87d7647c0345
Edmund Little <edlittle96@gmail.com>
2022-02-18 19:17:15 +0000

    Flake compliance

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-# # -- Options for HTML output -------------------------------------------------
+# # -- Options for HTML output -----------------------------------------------
 
 # # The theme to use for HTML and HTML Help pages.  See the documentation for
 # # a list of builtin themes.
