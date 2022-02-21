d74c37ebca735a9778e698d334ff62447176dfc5
Edmund Little <edlittle96@gmail.com>
2022-02-21 17:39:50 +0000

    Add nutations

--- spinpynamics.py
+++ spinpynamics.py
@@ -1,6 +1,7 @@
 from itertools import product
 import numpy as np
 from scipy.linalg import expm
+import matplotlib.pyplot as plt
 
 
 class ProductOperator():
@@ -26,6 +27,7 @@ class ProductOperator():
 
     def set_coef(self, coef):
         self.coef = coef
+        return self
 
     def get_coef(self):
         return self.coef
@@ -70,6 +72,40 @@ class ProductOperator():
             coef_label = coef_pattern.format(self.coef)
         # Combine and return
         return coef_label + op_label
+
+    def commutator(self, op, *, norm=False):
+        """Calculate commutator with another product operator
+
+        op is treated as 'operator B'
+
+        Parameters
+        ----------
+        op : ProductOperator
+        norm : bool
+            If True, commutator returned with coef=1.0
+        Returns
+        -------
+        op_t : ProductOperator
+            Commutator
+        """
+        # Calculate commutator
+        opA = self.as_matrix()
+        opB = op.as_matrix()
+        com_mat = np.matmul(opA, opB) - np.matmul(opB, opA)
+        # If zero return None
+        if np.all(np.isclose(com_mat, 0.0)):
+            return None
+        else:
+            # Otherwise make spin operator
+            spin_op = SpinOperator.from_mat(com_mat, spins=self.spins)
+            if len(spin_op.ops) == 1:
+                # Ensure unit amplitude
+                op_t = list(spin_op.ops.values())[0]
+                if norm is True:
+                    op_t.coef = op_t.coef/np.abs(op_t.coef)
+                return op_t
+            else:
+                raise ValueError('Product operator commutator invalid.')
 
     def _gen_matrix_rep(self):
         """Generate matrix representation"""
@@ -134,7 +170,33 @@ class ProductOperator():
     def __add__(self, op):
         if isinstance(op, ProductOperator):
             if op.comps == self.comps:
-                self.set_coef(self.)
+                self.set_coef(self.coef + op.coef)
+                return self
+            else:
+                return SpinOperator([self, op])
+        if isinstance(op, SpinOperator):
+            return SpinOperator([self] + list(op.ops.values()))
+
+    def __sub__(self, op):
+        if isinstance(op, ProductOperator):
+            if op.comps == self.comps:
+                self.set_coef(self.coef - op.coef)
+                return self
+            else:
+                op.coef *= -1.0
+                return SpinOperator([self, op])
+        if isinstance(op, SpinOperator):
+            all_ops = [self]
+            for comps in op.ops.keys():
+                op = op.ops[comps]
+                op.set_coef(-1.0*op.get_coef())
+                all_ops.append()
+            return SpinOperator(all_ops)
+
+    def __mult__(self, val):
+        self.set_coef(val*self.coef)
+        return self
+
 
 class MultiOperatorMixin():
     """Methods relating to objects containing multiple ProductOperators.
@@ -142,6 +204,18 @@ class MultiOperatorMixin():
 
     def get_label(self):
         return self.as_string()
+
+    def get_coef_dict(self):
+        return {(comps, op.coef) for comps, op in self.ops.items()}
+
+    def get_coef_list(self):
+        return [op.coef for op in self.ops.values()]
+
+    def get_ops_dict(self):
+        return self.ops
+
+    def get_ops_list(self):
+        return list(self.ops.values())
 
     def as_matrix(self):
         """Return matrix representation of operator scaled by coef
@@ -195,6 +269,161 @@ class MultiOperatorMixin():
                 label += op.as_string(op_pattern, coef_pattern=coef_pattern)
         return label
 
+    def normalise(self):
+        """Normalises amplitudes of operator coeficients.
+
+        Parameters
+        ----------
+        self
+
+        Returns
+        -------
+        self
+        """
+        # Get coeficients
+        coef_array = np.array(self.get_coef_list())
+        # Get normalisation constants
+        euc_norm = np.sqrt(np.sum(coef_array*coef_array))
+        # Normalise array
+        coef_norm = coef_array*np.sqrt(2.0)/euc_norm
+        # Update coeficients
+        for coef_, op in zip(coef_norm, self.ops.values()):
+            op.set_coef(coef_)
+        return self
+
+    def expectation_value(self, op):
+        """Get expectation value of self with respect to op
+
+        Parameters
+        ----------
+        op : SpinOperator or ProductOperator
+
+        Returns
+        -------
+        val : float
+            Expectation value
+        """
+        val = np.trace(np.matmul(self.as_matrix(), op.as_matrix()))
+        norm = np.trace(np.matmul(op.as_matrix(), op.as_matrix()))
+        return val/norm
+
+    def nutate(self, rot_op):
+        """Brute force nutation of spin operator
+
+        Parameters
+        ----------
+        rot_op : ProductOperator or SpinOperator
+            Operator to nutate by
+
+        Returns
+        -------
+        op_t : SpinOperator
+            Rotated spin operator, as a new copy of the original spin operator
+        """
+        # If op is a product operator
+        if isinstance(rot_op, ProductOperator):
+            op_t = self._nut_prodop(rot_op)
+        # Otherwise if it's a spin operator
+        elif isinstance(rot_op, SpinOperator):
+            # If all components commute
+            if rot_op._all_commute() is True:
+                # Copy self
+                op_t = SpinOperator(self.get_ops_list())
+                # Loop through all components of rotation operator
+                for op in rot_op.get_ops_list():
+                    # And apply them in turn (order not important)
+                    op_t = op_t._nut_prodop(op)
+            # Otherwise brute force with propogators
+            else:
+                op_t = self._nut_propagator(rot_op)
+        return op_t
+
+    def _nut_prodop(self, rot_op):
+        """Nutate by product operator using commutators
+
+        Parameters
+        ----------
+        rot_op : ProductOperator
+            Operator to nutate by
+
+        Returns
+        -------
+        op_t : SpinOperator
+            Rotated spin operator, as a new copy of the original spin operator
+        """
+        # Loop through all ProductOperators
+        op_t = None
+        for op in self.ops.values():
+            # Make copy of operators with unit coeficients
+            op_ = ProductOperator(op.comps, spins=op.spins)
+            rot_ = ProductOperator(rot_op.comps, spins=rot_op.spins)
+            # Calculate commutator
+            com = op_.commutator(rot_, norm=True)
+            # If commutator is not zero
+            if com is not None:
+                # Commutator sign
+                com_sign = np.real(-com.coef/1j)
+                coef_a = np.cos(rot_op.coef)*op.coef
+                coef_b = np.sin(rot_op.coef)*op.coef*com_sign
+                # Create operators
+                op_a = ProductOperator(op.comps, coef=coef_a, spins=op.spins)
+                op_b = ProductOperator(com.comps, coef=coef_b, spins=op.spins)
+                # Add to temporary if significant
+                if ~np.isclose(coef_a, 0.0) and ~np.isclose(coef_b, 0.0):
+                    op_ = op_a + op_b
+                elif ~np.isclose(coef_a, 0.0) and np.isclose(coef_b, 0.0):
+                    op_ = op_a
+                elif np.isclose(coef_a, 0.0) and ~np.isclose(coef_b, 0.0):
+                    op_ = op_b
+            # If commutator is zero
+            else:
+                op_ = ProductOperator(op.comps, coef=op.coef, spins=op.spins)
+            # Add to total
+            if op_t is None:
+                op_t = op_
+            else:
+                op_t = op_t + op_
+        # Ensure spin operator
+        if isinstance(op_t, ProductOperator):
+            op_t = SpinOperator([op_t])
+        return op_t
+
+    def _nut_propagator(self, rot_op):
+        """Nutate by product operator using propogators
+
+        Parameters
+        ----------
+        rot_op : ProductOperator or SpinOperator
+            Operator to nutate by
+
+        Returns
+        -------
+        op_t : SpinOperator
+            Rotated spin operator, as a new copy of the original spin operator
+        """
+        # Calculate propogators (slow)
+        prop = expm(-1.0j*rot_op.as_matrix())
+        prop_t = expm(+1.0j*rot_op.as_matrix())
+        # Matrix representation of rotated operator
+        op_t_mat = np.matmul(np.matmul(prop, self.as_matrix()), prop_t)
+        # Convert to ProductOperators
+        return SpinOperator.from_mat(op_t_mat)
+
+    def _all_commute(self):
+        """Determine if all operators in SpinOperator commute
+
+        Returns
+        -------
+        tf : bool
+            True if all ProductOperators in SpinOperator commute
+        """
+        for i_opA, opA in enumerate(self.ops.values()):
+            subsequent_ops = list(self.ops.values())[i_opA + 1:]
+            for opB in subsequent_ops:
+                if opB.commutator(opA) is not None:
+                    return False
+        return True
+
     def _add_prodop(self, op, mod=1.0):
         """Add a cartesian product operator
 
@@ -242,9 +471,9 @@ class MultiOperatorMixin():
         self
         """
         # Loop through each component
-        for op in ops.values():
+        for op_ in op.ops.values():
             # Add to total
-            self._add_prodop(op, mod)
+            self._add_prodop(op_, mod)
         return self
 
     def __add__(self, op):
@@ -264,6 +493,43 @@ class MultiOperatorMixin():
     def __str__(self):
         return self.as_string()
 
+    def from_mat(mat, *, spins=None):
+        """Generate spin operator from matrix representation.
+
+        Parameters
+        ----------
+        mat: array-like
+            Matrix representation of operator
+        spins : array-like, shape (n_spins, )
+            Total spin quantum number of each spin centre. If None (default)
+            then all spins are assumed to be S = 1/2.
+        """
+        # Get number of spins
+        if spins is None:
+            n_spins = int(np.log2(mat.shape[0]))
+            # spins = np.ones((n_spins,))/2.0
+        else:
+            n_spins = len(spins)
+        # Get components of all Cartesian product operators as strings
+        all_comps = (product(['i', 'x', 'y', 'z'], repeat=n_spins))
+        all_comps = [''.join(comp) for comp in all_comps]
+        # Loop through all components
+        ops = []
+        for comps in all_comps:
+            # Make product operator
+            op = ProductOperator(comps, spins=spins)
+            # Get matrix representation
+            op_mat = op.as_matrix()
+            # Get normalisation constant
+            norm = np.trace(np.matmul(op_mat, op_mat))
+            # Get expectation value
+            coef = np.trace(np.matmul(mat, op_mat))/norm
+            # Store if non-zero
+            if ~np.isclose(coef, 0.0):
+                op.set_coef(coef)
+                ops.append(op)
+        return SpinOperator(ops)
+
 
 class SpinOperator(MultiOperatorMixin):
     """Spin operator (which can comprise linear combinations of product
@@ -279,16 +545,13 @@ class SpinOperator(MultiOperatorMixin):
     """
 
     def __init__(self, ops):
-        # Check consistency and copy into dictionary
+        # Get number of spins
         self.n_spins_ = ops[0].n_spins_
-        self.ops = {}
-        for op in ops:
-            # If
-            if op.n_spins_ == self.n_spins_:
-
-        self.n_spins_ = lops[0].comps)
-        self.ops_ = dict((op.comps, op) for op in ops)
-        self._all_comps =
+        # Start product operator dictionary
+        self.ops = {ops[0].comps: ops[0]}
+        # Add in any further product operators
+        for op in ops[1:]:
+            self._add_prodop(op)
 
     def set_coef(self, comps, coef):
         """Set coefficient of an operator
@@ -323,21 +586,42 @@ class SpinOperator(MultiOperatorMixin):
             mssg = '{:} is not a valid component string'.format(comps)
             raise ValueError(mssg)
 
-    def __add__(self, spin_operator):
-
-        # Copy current state
-        ops_ = dict(self.ops_.items())
-        # Loop through keys of spin operator to add
-
-# def Pulse(SpinOperator):
-#     """Set up a pulse
 
 if __name__ == '__main__':
-    # Make a spin operator and print it
-    mymat = SpinOperator([ProductOperator('zi'), ProductOperator('iz')])
-    print(mymat)
+    wA = 0.5
+    wB = -1.0
+    wAB = 0.1
+    x_90 = SpinOperator([ProductOperator('xi', coef=np.pi/2),
+                         ProductOperator('ix', coef=np.pi/2)])
+    x_180 = SpinOperator([ProductOperator('xi', coef=np.pi),
+                          ProductOperator('ix', coef=np.pi)])
 
+    X = np.linspace(0, 20*np.pi, 101)
+    sig = np.zeros((X.size, 4), dtype=np.complex128)
+    rho0 = SpinOperator([ProductOperator('zi'), ProductOperator('iz')])
+    rho0plus = rho0.nutate(x_90)
 
+    for i in range(X.size):
+        t = X[i]
+        tau = SpinOperator([ProductOperator('zi', coef=wA*t),
+                            ProductOperator('iz', coef=wB*t),
+                            ProductOperator('zz', coef=wAB*t)])
+        rhotauminus = rho0plus.nutate(tau)
+        rhotauplus = rhotauminus.nutate(x_180)
+        rhotwotau = rhotauplus.nutate(tau)
+        sig[i, 0] = rhotwotau.expectation_value(ProductOperator('xz'))
+        sig[i, 1] = rhotwotau.expectation_value(ProductOperator('yi'))
+        sig[i, 2] = rhotwotau.expectation_value(ProductOperator('zx'))
+        sig[i, 3] = rhotwotau.expectation_value(ProductOperator('iy'))
+    fig, ax = plt.subplots()
+    ax.plot(X, sig[:, 0], label='Sxz')
+    ax.plot(X, sig[:, 1], label='Syi')
+    ax.legend()
+    fig, ax = plt.subplots()
+    ax.plot(X, sig[:, 2], label='Szx')
+    ax.plot(X, sig[:, 3], label='Siy')
+    ax.legend()
+    plt.show()
 # class CartesianSpinOperator():
 #     """Spin operator expressed as sum of Cartesian components
 
