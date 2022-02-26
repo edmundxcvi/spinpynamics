11339b96bba436be567f221750bb49f6cfcfddbe
Edmund Little <edlittle96@gmail.com>
2022-02-26 11:07:53 +0000

    Remove static from docs

--- docs/conf.py
+++ docs/conf.py
@@ -80,8 +80,3 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Override some CSS settings
 html_css_files = ["readthedocs-custom.css"]
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
